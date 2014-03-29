490b5a1a62f4a7cab568286c3a0e8f42ffa56631
Yan Barreta <augustianne.barreta@gmail.com>
2014-03-29 13:14:12 +0800

    Added RandomCharacter generator class

---
+++ Generator/RandomCharacterGenerator.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of SiteUtility.
+ *
+ * Yan Barreta <augustianne.barreta@gmail.com>
+ *
+ * For the full copyright and license information,please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Site\UtilityBundle\Generator;
+
+use \Exception;
+
+/**
+* Generates random characters
+*
+* @author  Yan Barreta <augustianne.barreta@gmail.com>
+* @version dated: March 29,2014 12:39:26
+*/
+class RandomCharacterGenerator
+{
+
+    const ALPHA_NUMERIC = 1;
+    const ALPHA_NUMERIC_LOWERCASE = 2;
+    const ALPHA_NUMERIC_UPPERCASE = 3;
+    
+    private $characterSet = array(
+        self::ALPHA_NUMERIC => array(
+            '0','1','2','3','4','5','6','7','8','9',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        ),
+        self::ALPHA_NUMERIC_UPPERCASE => array(
+            '0','1','2','3','4','5','6','7','8','9',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        ),
+        self::ALPHA_NUMERIC_LOWERCASE => array(
+            '0','1','2','3','4','5','6','7','8','9',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+        )
+    );
+
+    /**
+     * Returns character set by type
+     *
+     * @param int $type, type of characters 
+     * @return array
+     */
+    public function getCharacterSet($type)
+    {
+        if (!isset($this->characterSet[$type])) {
+            throw new Exception('Character set not supported.');
+        }
+
+        return $this->characterSet;
+    }
+
+    /**
+     * Generates random characters
+     *
+     * @param int $length, how long will the generated chracters be
+     * @param int $type, type of characters to be generated
+     * @return string
+     */
+    public function generate($length, $type=self::ALPHA_NUMERIC)
+    {
+        $generatedString = array();
+
+        if (!isset($this->characterSet[$type])) {
+            throw new Exception('Character set not supported.');
+        }
+
+        $characterSet = $this->characterSet[$type];
+        $upperLimit = count($characterSet)-1;
+        while (count($generatedString) < $length) {
+            $generatedString[] = $characterSet[rand(0, $upperLimit)];
+        }
+
+        return implode('', $generatedString);
+    }
+}
---
+++ Tests/Unit/Generator/RandomCharacterGeneratorTest.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of SiteUtility.
+ *
+ * Yan Barreta <augustianne.barreta@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Site\UtilityBundle\Tests\Unit\Generator;
+
+use Site\UtilityBundle\Generator\RandomCharacterGenerator;
+
+/**
+ * Unit test for RandomCharacterGenerator
+ *
+ * @author  Yan Barreta
+ * @version dated: March 29,2014 12:39:26
+ */
+class RandomCharacterGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+
+	private $sut;
+
+	public function setup()
+	{
+		$this->sut = new RandomCharacterGenerator();
+	}
+
+	public function getInput()
+	{
+		return array(
+			array(7, RandomCharacterGenerator::ALPHA_NUMERIC),
+			array(10, RandomCharacterGenerator::ALPHA_NUMERIC),
+			array(5, RandomCharacterGenerator::ALPHA_NUMERIC)
+		);
+	}
+
+	public function getInputLowerCase()
+	{
+		return array(
+			array(7, RandomCharacterGenerator::ALPHA_NUMERIC_LOWERCASE),
+			array(10, RandomCharacterGenerator::ALPHA_NUMERIC_LOWERCASE),
+			array(5, RandomCharacterGenerator::ALPHA_NUMERIC_LOWERCASE)
+		);
+	}
+
+	public function getInputUpperCase()
+	{
+		return array(
+			array(7, RandomCharacterGenerator::ALPHA_NUMERIC_UPPERCASE),
+			array(10, RandomCharacterGenerator::ALPHA_NUMERIC_UPPERCASE),
+			array(5, RandomCharacterGenerator::ALPHA_NUMERIC_UPPERCASE)
+		);
+	}
+
+	/**
+     * @covers Site\UtilityBundle\Generator\RandomCharacterGenerator::generate
+     * @dataProvider getInput
+     */
+	public function testGenerateAlphanumeric($length, $type)
+	{
+		$randomString = $this->sut->generate($length, $type);
+		var_dump($randomString);
+		$this->assertEquals($length, strlen($randomString));
+		$this->assertTrue(ctype_alnum($randomString));
+	}
+
+	/**
+     * @covers Site\UtilityBundle\Generator\RandomCharacterGenerator::generate
+     * @dataProvider getInputLowerCase
+     */
+	public function testGenerateAlphanumericLowerCase($length, $type)
+	{
+		$randomString = $this->sut->generate($length, $type);
+		var_dump($randomString);
+		$this->assertEquals(strtolower($randomString), $randomString);
+		$this->assertEquals($length, strlen($randomString));
+		$this->assertTrue(ctype_alnum($randomString));
+	}
+
+		/**
+     * @covers Site\UtilityBundle\Generator\RandomCharacterGenerator::generate
+     * @dataProvider getInputUpperCase
+     */
+	public function testGenerateAlphanumericUpperCase($length, $type)
+	{
+		$randomString = $this->sut->generate($length, $type);
+		var_dump($randomString);
+		$this->assertEquals(strtoupper($randomString), $randomString);
+		$this->assertEquals($length, strlen($randomString));
+		$this->assertTrue(ctype_alnum($randomString));
+	}
+}
